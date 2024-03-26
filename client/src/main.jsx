536425f4fa54b7b6b278d95b58f968632e969bc6
viki2207 <vidhishah2207@gmail.com>
2024-03-26 23:49:44 +1100

    persistor correction

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,7 @@ import { store, persistor } from "./redux/store.jsx";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <PersistGate persistor={personalbar}>
+  <PersistGate persistor={persistor}>
     <Provider store={store}>
       <App />
     </Provider>
